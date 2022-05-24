b9f82f77a8d57ef1dd337a8e20bb92070bd3de95
Fitness Club <shivanibhadauria921997@gmail.com>
2022-05-24 07:22:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let sounds =['happy', 'lion', 'rain', 'laugh' ]
+
+sounds.forEach( sound => {
+ let btn = document.createElement('button')
+ btn.classList.add('btn')
+
+ btn.innerText = sound
+
+ btn.addEventListener('click', () => {
+
+    stopSongs()
+     document.getElementById(sound).play()}
+
+     )
+
+ document.getElementById('buttons').appendChild(btn)
+
+
+ 
+
+})
+
+function stopSongs() {
+
+    sounds.forEach(sound =>{
+    let song = document.getElementById(sound)
+
+    song.pause()
+    song.currentTime =0;
+
+})
+
+    
+}
+
